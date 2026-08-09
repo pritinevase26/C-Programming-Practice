3cc569e6981fc9ba3627089ba5997c1cb0e30cdb
Priti Nevase <pritinevase26@gmail.com>
2026-08-09 23:42:13 +0530

    Add selection sort implementation

---
+++ selection_sort.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main(){
+int i,j,n,temp;
+printf("Enter the size of array elments:");
+scanf("%d",&n);
+int a[n];
+printf("Enter the array elements :");
+for(i=0;i<n;i++){
+        scanf("%d",&a[i]);
+}
+for(i=0;i<n-1;i++){
+    int min=i;
+    for(j=i+1;j<n;j++){
+        if(a[j]<a[min]){
+            min=j;
+        }
+    }
+    if (min!=i){
+        temp=a[i];
+        a[i]=a[min];
+        a[min]=temp;
+    }
+}
+
+printf("Sorted array: ");
+    for(i = 0; i < n; i++){
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+    return 0;
+}
+
